5d5516bcf3b2c12a7c2cf2443b6d4d213d060a85
Yanbing Chen <2000022772@alumni.pku.edu.cn>
2023-04-17 02:09:22 -0700

    Add socket-io on server, the client-side code using JavaScript is unfinished

--- flaskProject/command.py
+++ flaskProject/command.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import pymongo
 import json
 import uuid
+from flask_socketio import SocketIO, emit
 
 client = pymongo.MongoClient("localhost", 27017)
 # Define database "project" and collection in the database named "emp"
@@ -9,24 +10,15 @@ db_test = client.project
 collection = db_test.emp
 
 app = Flask(__name__)
-
-'''This part below is to support command line. Using curl -X to manipulate the data'''
+# Create the SocketIO object after creating the Flask app
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 # GET and filtering function: orderBy=”$key”/”$value”/”name”, limitToFirst/Last, equalTo, startAt/endAt for the whole data
-# eg. curl -X GET 'http://localhost:5000/employees.json' to get the whole data
-# or using filtering function eg. curl -X GET 'http://localhost:5000/employees.json?orderBy="Full%20Name"&limitToFirst=5' ; curl -X GET 'http://localhost:5000/employees.json?orderBy="Bonus%20%25"&equalTo="25%"'
-@app.route('/employees.json', methods=['GET'])
-def get_employees():
-    orderBy = request.args.get('orderBy', default=None)
-    limitToFirst = int(request.args.get('limitToFirst', default=0))
-    limitToLast = int(request.args.get('limitToLast', default=0))
-    equalTo = request.args.get('equalTo', default=None)
-    startAt = request.args.get('startAt', default=None)
-    endAt = request.args.get('endAt', default=None)
 
+def ws_get_employees(orderBy=None, limitToFirst=0, limitToLast=0, equalTo=None, startAt=None, endAt=None):
     # check if orderBy is not defined while other query parameters are present
     if not orderBy and (limitToFirst or limitToLast or equalTo or startAt or endAt):
-        return jsonify({"error": "orderBy must be defined when other query parameters are defined"}), 400
+        return {"error": "orderBy must be defined when other query parameters are defined"}
 
     if orderBy:
         orderBy = orderBy.replace('"', '')
@@ -75,28 +67,47 @@ def get_employees():
         query.append({"$project": {"_id": 0}})
 
         emp = [i for i in collection.aggregate(query)]
-        return jsonify(emp)
     else:
         emp = []
         a = collection.find({}, {"_id": 0})
         for i in a:
             emp.append(i)
-        return jsonify(emp)
+    return emp
+
+# command line # eg. curl -X GET 'http://localhost:5000/employees.json' to get the whole data
+# or using filtering function eg. curl -X GET 'http://localhost:5000/employees.json?orderBy="Full%20Name"&limitToFirst=5' ; curl -X GET 'http://localhost:5000/employees.json?orderBy="Bonus%20%25"&equalTo="25%"'
+@app.route('/employees.json', methods=['GET'])
+def get_employees():
+    orderBy = request.args.get('orderBy', default=None)
+    limitToFirst = int(request.args.get('limitToFirst', default=0))
+    limitToLast = int(request.args.get('limitToLast', default=0))
+    equalTo = request.args.get('equalTo', default=None)
+    startAt = request.args.get('startAt', default=None)
+    endAt = request.args.get('endAt', default=None)
+    result = ws_get_employees(orderBy, limitToFirst, limitToLast, equalTo, startAt, endAt)
+    # Check if the result is an error and handle it
+    if isinstance(result, tuple) and len(result) == 2 and isinstance(result[1], int):
+        return jsonify(result[0]), result[1]
+
+    return jsonify(result)
+
+# WebSocket event handler for getting employees
+@socketio.on('get_employees')
+def handle_get_employees(data):
+    orderBy = data.get('orderBy')
+    limitToFirst = int(data.get('limitToFirst', 0))
+    limitToLast = int(data.get('limitToLast', 0))
+    equalTo = data.get('equalTo')
+    startAt = data.get('startAt')
+    endAt = data.get('endAt')
+
+    emp = ws_get_employees(orderBy, limitToFirst, limitToLast, equalTo, startAt, endAt)
+    emit('employees_received', emp, broadcast=True)
+
 
 # PUT for adding new employee or overwriting employee's information by its EEID
-# eg. curl -X PUT 'http://localhost:5000/employees/E100000.json' -d '{"Full Name": "George Bush","Job Title": "President","Department": "Politics","Business Unit": "Overall Development","Gender": "Male","Age": 94,"Hire Date": "1/20/1989","Annual Salary": "$400,000","Bonus %": "15%","Country": "United States","Exit Date": "01/20/1993"}'
-@app.route('/employees/<string:eeid>.json', methods=['PUT'])
-def put_employee(eeid):
+def ws_put_employee(eeid, dict):
     try:
-        # try to parse request data as JSON
-        data = request.get_json(silent=True)
-        # if parsing failed or data is empty, try to parse request data as form data
-        # for curl without -H "Content-Type: application/json" in the format as Firebase, it cannot be recognized as json
-        # It will be retrieve as a key(though I don't know why). Try to retrieve the key-value pairs
-        if not data:
-            data = request.form.to_dict()
-            for key, value in data.items():
-                dict = json.loads(key)
         # To adjust the order of keys
         data = {}
         data['EEID'] = eeid
@@ -107,77 +118,149 @@ def put_employee(eeid):
             # reference: Update Operation part on https://www.mongodb.com/docs/v4.4/crud/
             # As it can replace the whole document, it's corresponded to the overwrite function when data exists.
             collection.replace_one({'EEID': eeid}, data)
-            return jsonify({"message": f"Employee {eeid} updated successfully."})
+            return {"message": f"Employee {eeid} updated successfully."}
         else:
             # insert new employee with eeid
             collection.insert_one(data)
-            return jsonify({"message": f"New employee {eeid} added successfully."})
+            return {"message": f"Employee {eeid} added successfully."}
     except Exception as e:
-        print(e)
-        return jsonify({"error": str(e)}), 400
+        return {"error": str(e)}
+
+# copmmand line eg. curl -X PUT 'http://localhost:5000/employees/E100000.json' -d '{"Full Name": "George Bush","Job Title": "President","Department": "Politics","Business Unit": "Overall Development","Gender": "Male","Age": 94,"Hire Date": "1/20/1989","Annual Salary": "$400,000","Bonus %": "15%","Country": "United States","Exit Date": "01/20/1993"}'
+@app.route('/employees/<string:eeid>.json', methods=['PUT'])
+def put_employee(eeid):
+    # try to parse request data as JSON
+    data = request.get_json(silent=True)
+    # if parsing failed or data is empty, try to parse request data as form data
+    # for curl without -H "Content-Type: application/json" in the format as Firebase, it cannot be recognized as json
+    # It will be retrieved as a key(though I don't know why). Try to retrieve the key-value pairs
+    if not data:
+        data = request.form.to_dict()
+        for key, value in data.items():
+            dict = json.loads(key)
+    result = ws_put_employee(eeid, dict)
+    if 'error' in result:
+        return jsonify(result), 400
+    return jsonify(result)
+
+# WebSocket event handler for adding or overwriting employees
+@socketio.on('put_employee')
+def handle_put_employee(data):
+    eeid = data.get('eeid')
+    # employee_data needs to be a dictionary
+    dict = data.get('employee_data')
+
+    result = ws_put_employee(eeid, dict)
+    emit('employee_put', result, broadcast=True)
+
 
 # POST: Create a new employee without specifying EEID (server generates EEID automatically).
-# eg. curl -X POST 'http://localhost:5000/employees.json' -d '{"Full Name": "John Doe","Job Title": "Developer","Department": "IT","Business Unit": "Software","Gender": "Male","Age": 28,"Hire Date": "10/01/2017","Annual Salary": "$80,000","Bonus %": "5%","Country": "United States","Exit Date": ""}'
-@app.route('/employees.json', methods=['POST'])
-def post_employee():
+def ws_post_employee(dict):
     try:
-        data = request.get_json(silent=True)
-        if not data:
-            data = request.form.to_dict()
-            for key, value in data.items():
-                dict = json.loads(key)
-
         # Generate a unique EEID using the uuid library
         eeid = f'E{str(uuid.uuid4())[:6].upper()}'
         data = {}
         data['EEID'] = eeid
         data.update(dict)
         collection.insert_one(data)
-        return jsonify({"message": f"New employee {eeid} added successfully."})
+        return {"message": f"New employee {eeid} added successfully."}
     except Exception as e:
-        print(e)
-        return jsonify({"error": str(e)}), 400
+        return {"error": str(e)}
+
+# command line eg. curl -X POST 'http://localhost:5000/employees.json' -d '{"Full Name": "John Doe","Job Title": "Developer","Department": "IT","Business Unit": "Software","Gender": "Male","Age": 28,"Hire Date": "10/01/2017","Annual Salary": "$80,000","Bonus %": "5%","Country": "United States","Exit Date": ""}'
+@app.route('/employees.json', methods=['POST'])
+def post_employee():
+    data = request.get_json(silent=True)
+    if not data:
+        data = request.form.to_dict()
+        for key, value in data.items():
+            dict = json.loads(key)
+
+    result = ws_post_employee(dict)
+    if 'error' in result:
+        return jsonify(result), 400
+    return jsonify(result)
+
+# WebSocket event handler for posting new employees
+@socketio.on('post_employee')
+def handle_post_employee(data):
+    result = post_employee(data)
+    emit('employee_created', result, broadcast=True)
+
 
 # PATCH: Update specific fields of an existing employee by their EEID.
-# eg. curl -X PATCH 'http://localhost:5000/employees/E100000.json' -d '{"Business Unit": "Overall Leadership", "Bonus %": "20%"}'
-@app.route('/employees/<string:eeid>.json', methods=['PATCH'])
-def patch_employee(eeid):
+def ws_patch_employee(eeid, data):
     try:
-        data = request.get_json(silent=True)
-        if not data:
-            data = request.form.to_dict()
-            for key, value in data.items():
-                dict = json.loads(key)
-            data = dict
-
         # update the specified fields or create them if they don't exist
         # insert a new document with the EEID and data if the document doesn't exist
         result = collection.update_one({'EEID': eeid}, {'$set': data}, upsert=True)
 
         if result.matched_count > 0:
-            return jsonify({"message": f"Employee {eeid} updated successfully."})
+            return {"message": f"Employee {eeid} updated successfully."}
         else:
-            return jsonify({"error": f"Employee {eeid} not found but upserted successfully"}), 404
+            return {"error": f"Employee {eeid} not found but upserted successfully"}, 404
     except Exception as e:
-        print(e)
-        return jsonify({"error": str(e)}), 400
+        return {"error": str(e)}, 400
+
+# command line eg. curl -X PATCH 'http://localhost:5000/employees/E100000.json' -d '{"Business Unit": "Overall Leadership", "Bonus %": "20%"}'
+@app.route('/employees/<string:eeid>.json', methods=['PATCH'])
+def patch_employee(eeid):
+    data = request.get_json(silent=True)
+    if not data:
+        data = request.form.to_dict()
+        for key, value in data.items():
+            dict = json.loads(key)
+        data = dict
+    result = ws_patch_employee(eeid, data)
+    if isinstance(result, tuple) and len(result) == 2 and isinstance(result[1], int):
+        return jsonify(result[0]), result[1]
+    return jsonify(result)
+
+# WebSocket event handler for patching employee
+@socketio.on('patch_employee')
+def handle_patch_employee(data):
+    eeid = data.get('eeid')
+    # employee_data needs to be a dictionary
+    data = data.get('employee_data')
+
+    result = ws_put_employee(eeid, data)
+    emit('employee_patched', result, broadcast=True)
 
 # DELETE: Remove an existing employee by their EEID.
-# eg. curl -X DELETE 'http://localhost:5000/employees/E100000.json'
-@app.route('/employees/<string:eeid>.json', methods=['DELETE'])
-def delete_employee(eeid):
+def ws_delete_employee(eeid):
     try:
         # Check if employee with eeid exists
         result = collection.delete_one({'EEID': eeid})
         count = result.deleted_count
         if count > 0:
-            return jsonify({"message": f"Employee {eeid} deleted successfully, {count} record of employee deleted"})
+            return {"message": f"Employee {eeid} deleted successfully, {count} record of employee deleted"}
         else:
-            return jsonify({"error": f"Employee {eeid} not found."}), 404
+            return {"error": f"Employee {eeid} not found."}, 404
     except Exception as e:
-        print(e)
-        return jsonify({"error": str(e)}), 400
+        return {"error": str(e)}, 400
 
+# eg. curl -X DELETE 'http://localhost:5000/employees/E100000.json'
+@app.route('/employees/<string:eeid>.json', methods=['DELETE'])
+def delete_employee(eeid):
+    result = ws_delete_employee(eeid)
+    if isinstance(result, tuple) and len(result) == 2 and isinstance(result[1], int):
+        return jsonify(result[0]), result[1]
+    return jsonify(result)
+
+# WebSocket event handler for deleting employee
+@socketio.on('delete_employee')
+def handle_delete_employee(eeid):
+    result = ws_delete_employee(eeid)
+    emit('employee_deleted', result, broadcast=True)
+
+# WebSocket basic event handlers
+@socketio.on('connect')
+def handle_connect():
+    print('Client connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
